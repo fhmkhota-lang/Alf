37c5195cd5718d2b438528e1c4a31b2086729c17
fhmkhota-lang <fhmkhota@gmail.com>
2026-05-26 12:21:40 +0200

    Add Vite React app for live Vercel deployment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Cormorant Garamond', 'Georgia', 'serif'],
+        mono: ['JetBrains Mono', 'Menlo', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
